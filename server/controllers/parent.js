d4e599c5670919df21dc2cd495536336c8b9fdbf
meriam <hamdaoui.meriam85@gmail.com>
2022-06-09 21:27:37 +0200

    verify email existance

--- server/controllers/parent.js
+++ server/controllers/parent.js
@@ -1,15 +1,20 @@
 const { Parent, Child } = require("../models/parent");
 
 exports.signup = async (req, res) => {
+  const { email, password } = req.body;
   try {
     //search if our user already exists
-    //const parentExists = await Parent.findOne({email});
+    const parentExists = await Parent.findOne({ email });
     //exists
-    //if(parentExists){return res.status(400).send("this email already exists")}
+    if (parentExists) {
+      return res.status(400).send("this email already exists");
+    }
     //doesn't exist
     const newParent = await new Parent(req.body);
-
-    const newchild = await new Child(newParent.child[0]);
+    let newchild = {};
+    newParent.child.forEach((element) => {
+      newchild = new Child(element);
+    });
 
     await newParent.save();
     await newchild.save();
