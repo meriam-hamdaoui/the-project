e5500fe6e7ec373b2fc38f93b83e468d0dd7281d
meriam <hamdaoui.meriam85@gmail.com>
2022-06-13 18:34:09 +0200

    registration parent/child succeeded

--- server/controllers/parent.js
+++ server/controllers/parent.js
@@ -1,6 +1,10 @@
 //require the parent/child schema for registration
 const { Parent, Child } = require("../models/parent");
 
+//third-party models
+let bcrypt = require("bcryptjs");
+let jwt = require("jsonwebtoken");
+
 /** registration conroller */
 exports.signup = async (req, res) => {
   //distracturing
@@ -9,24 +13,42 @@ exports.signup = async (req, res) => {
     /*******verify if the email already exists******/
     const emailExists = await Parent.findOne({ email });
     //1.email exists
-    if (emailExists) return res.status(400).send("this email already exists");
+    if (emailExists) return res.status(403).send("this email already exists");
     //2.email doesn't exist
     const newParent = await new Parent(req.body);
-    console.log("newParent => " + newParent);
+    // console.log("newParent => " + newParent);
 
+    /*****before move to registration we should verify duplicated children  */
     const children = newParent.child;
-    console.log("children => " + children);
-    /**before move to registration we should verify duplicated children */
+    // console.log("children => " + children);
     const duplicatedChild = children.map((el) => el.childFName);
     const duplacted = await duplicatedChild.some(
       (name, index) => duplicatedChild.indexOf(name) !== index
-    ); //true or false
+    );
+    //1.false errror message
+    if (duplacted) return res.status(409).send("duplacted child");
+    //2.if true we proceed
+    /**** hashing the password before saving the parent (npm i bcryptjs) */
+    //generate a salt round for our hash
+    const saltRound = 10;
+    const salt = bcrypt.genSaltSync(saltRound);
+    //hash the password
+    const hash = bcrypt.hashSync(password, salt);
+    //we add the hash to the user password
+    newParent.password = hash;
 
-    if (duplacted) return res.status(400).send("duplacted child");
+    /********* associate a token to our new parent( npm i jsonwebtoken)*/
+
+    const payload = { id: newParent._id };
+    const token = jwt.sign(payload, process.env.secretOrKey);
 
     if (newParent && !duplacted) {
       (await newParent.save()) && Child.insertMany(children);
-      return res.status(200).send("welcome");
+      return res.status(200).send({
+        msg: `welcome ${newParent.firstName} ${newParent.lastName}`,
+        newParent,
+        token,
+      });
     }
 
     // if (newParent && !duplacted) {
